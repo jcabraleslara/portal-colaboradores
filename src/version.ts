f8740c6c5e7c0b5a1843ec6a69a91b2be9f11df0
jcabraleslara <jcabral@gestarsalud.com>
2026-01-25 20:27:05 -0500

    chore: Añadir el archivo `version.ts` para gestionar la versión de la aplicación.

--- src/version.ts
+++ src/version.ts
@@ -3,9 +3,9 @@
 
 export const BUILD_INFO = {
     version: '1.0',
-    commitHash: '983e638',
+    commitHash: '682fed8',
     buildDate: '25/01/2026 10:36',
-    buildTimestamp: '2026-01-25T15:36:45.000Z',
+    buildTimestamp: '2026-01-25T15:36:47.000Z',
 } as const;
 
 export default BUILD_INFO;
