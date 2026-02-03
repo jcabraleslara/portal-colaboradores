980c5016695df0220169320aa689431f4e1fe087
jcabraleslara <jcabral@gestarsalud.com>
2026-02-03 18:07:19 -0500

    chore: update version info [skip ci]

--- src/version.ts
+++ src/version.ts
@@ -1,11 +1,11 @@
 // Auto-generado - NO EDITAR MANUALMENTE
-// Generado el: 03/02/2026 17:58
+// Generado el: 03/02/2026 18:07
 
 export const BUILD_INFO = {
     version: '1.0',
-    commitHash: '70691d5',
-    buildDate: '03/02/2026 17:58',
-    buildTimestamp: '2026-02-03T22:58:35.000Z',
+    commitHash: '2a5e6b0',
+    buildDate: '03/02/2026 18:07',
+    buildTimestamp: '2026-02-03T23:07:12.000Z',
 } as const;
 
 export default BUILD_INFO;
