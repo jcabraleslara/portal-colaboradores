550eaab6cdd7c62439550d73ac2d546c52b1b322
Jhonattan Cabrales Lara <jhonlara@gmail.com>
2026-02-23 17:31:41 -0500

    chore: update version info [skip ci]

--- src/version.ts
+++ src/version.ts
@@ -1,11 +1,11 @@
 // Auto-generado - NO EDITAR MANUALMENTE
-// Generado el: 23/02/2026 16:07
+// Generado el: 23/02/2026 17:31
 
 export const BUILD_INFO = {
     version: '1.0',
-    commitHash: '2d143e5',
-    buildDate: '23/02/2026 16:07',
-    buildTimestamp: '2026-02-23T21:07:31.000Z',
+    commitHash: 'a3c675c',
+    buildDate: '23/02/2026 17:31',
+    buildTimestamp: '2026-02-23T22:31:36.000Z',
 } as const;
 
 export default BUILD_INFO;
