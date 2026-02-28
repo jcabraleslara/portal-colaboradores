eec5ba12f529ec6c34ef95b379a8461e7096c495
jcabraleslara <jhonlara@gmail.com>
2026-02-28 10:56:41 -0500

    chore: update version info [skip ci]

--- src/version.ts
+++ src/version.ts
@@ -1,11 +1,11 @@
 // Auto-generado - NO EDITAR MANUALMENTE
-// Generado el: 28/02/2026 10:53
+// Generado el: 28/02/2026 10:56
 
 export const BUILD_INFO = {
     version: '1.0',
-    commitHash: 'c4ccf7a',
-    buildDate: '28/02/2026 10:53',
-    buildTimestamp: '2026-02-28T15:53:16.000Z',
+    commitHash: 'e406f4d',
+    buildDate: '28/02/2026 10:56',
+    buildTimestamp: '2026-02-28T15:56:40.000Z',
 } as const;
 
 export default BUILD_INFO;
